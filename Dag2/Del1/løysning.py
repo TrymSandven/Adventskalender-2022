6cd28952c899c269f56d42585105372ea4b6ca2a
TrymSandven <64443177+TrymSandven@users.noreply.github.com>
2022-12-02 23:09:21 +0100

    Fjerna mellomrom i nvn

---
+++ Dag2/Del1/løysning.py
@@ -0,0 +1,42 @@
+import os
+import os.path
+
+def steinSaksPapir():
+    meg = {'X':1, 'Y':2, 'Z':3}
+    total = 0
+    
+    try:
+        fila=open('input.txt')
+    except:
+        print('Kan ikkje opna filen')
+
+    for linje in fila:
+        total += meg[linje[-2]]
+
+        if linje[0] == 'A':
+            if linje[-2] == 'Y':
+                total += 6
+            elif linje[-2] == 'X':
+                total += 3
+            else:
+                pass
+        
+        elif linje[0] == 'B':
+            if linje[-2] == 'Z':
+                total += 6
+            elif linje[-2] == 'Y':
+                total += 3
+            else:
+                pass
+        
+        elif linje[0] == 'C':
+            if linje[-2] == 'X':
+                total += 6
+            elif linje[-2] == 'Z':
+                total += 3
+            else:
+                pass
+
+    print(total)
+
+steinSaksPapir()
