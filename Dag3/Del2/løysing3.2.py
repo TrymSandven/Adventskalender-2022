bcd56ba09820515388448e1aba176834732811a3
TrymSandven <64443177+TrymSandven@users.noreply.github.com>
2022-12-10 14:22:43 +0100

    Dag 3

--- Dag3/Del2/løysing3.2.py
+++ Dag3/Del2/løysing3.2.py
@@ -1,1 +1,42 @@
-print('Test')
+import os
+import os.path
+import time
+import string
+
+def pakke_sort():
+    verdi = dict()
+    total = 0
+    liste = []
+
+#Leser inn fila      
+    try:
+        fila=open('./Dag3/Del2/input.txt')
+    except:
+        print('Kan ikkje opna filen')
+ 
+    #Tildeler ein verdi til bokstaver a-Z frå 1-52 
+    for index, letter in enumerate(string.ascii_letters):
+        verdi[letter] = index + 1
+
+#Legger heile tekstfila i ei liste og legger 3 og 3 inn i ei subliste
+    for line in fila:
+        line = line.strip()
+        liste.append(line)
+    liste = [liste[i:i+3] for i in range(0,len(liste),3)]
+        
+    #Tilldeler kvart element i sublistene sin eigen variabel
+    for i in range(0, 100):
+        del1 = set(liste[i][0])
+        del2 = set(liste[i][1])
+        del3 = set(liste[i][2])
+        
+        #Finner felles bokstav i del1 og del2, deretter i del3
+        common_characters = ''.join(set(del1).intersection(del2))
+        common_characters = ''.join(set(common_characters).intersection(del3))
+        total += verdi[common_characters]
+    print(total)
+
+
+
+
+pakke_sort()
